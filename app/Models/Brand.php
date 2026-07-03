cc6bacd789472a4daf06b1cbf00e332b64587a27
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-07-02 21:22:58 -0600

    feat: add Brand model and relationships

---
+++ app/Models/Brand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $table = 'brands';
+
+    protected $fillable = [
+        'name',
+        'logo',
+        'country_origin',
+        'industry',
+        'description'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
