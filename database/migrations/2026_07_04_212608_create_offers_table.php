ae0c8c32c66c4e5724d93b1122f39721b8161477
lencet-344 <odoricozeleherrera@gmail.com>
2026-07-04 15:36:15 -0600

    feat: create migration offers

---
+++ database/migrations/2026_07_04_212608_create_offers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("title",30);
+            $table->string("type_offer",30);
+            $table->decimal("discount", 8, 2);
+            $table->string("description",100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
