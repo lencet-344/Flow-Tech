bffef35c5f23640e4a92aaedd233902fde8aaa12
lencet-344 <odoricozeleherrera@gmail.com>
2026-07-04 15:34:44 -0600

    feat: create migration contact request

---
+++ database/migrations/2026_07_04_213201_create_contact_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_requests', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("name", 30);
+            $table->string("email", 30)->unique();
+            $table->integer("telephone", 8)->unique();
+            $table->string("location", 30);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_requests');
+    }
+};
