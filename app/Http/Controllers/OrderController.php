d4c98600355dd5ec9b603ac1b7a624c6b9751f60
lencet-344 <odoricozeleherrera@gmail.com>
2026-07-11 09:02:49 -0600

    feat: create controller order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;  
 use App\Http\Request\OrderRequest;
-
+use App\Models\Buy_verification;
+use App\Models\User;
 use Illuminate\http\redirectResponse;
 use Illuminate\view\View;
 
@@ -16,7 +17,8 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::with("user")->get();
+        return view("orders.index", compact("orders"));
     }
 
     /**
@@ -24,23 +26,28 @@ class OrderController extends Controller
      */
     public function create()
     {
-        //
+        $order = new Order();
+        $users = User::all();
+        $buy_verifications = Buy_verification::class();
+        return view('orders.create',compact('order','users', 'buy_verifications'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(OrderRequest $request)
     {
-        //
+        Order::create($request->validated());
+        return redirect()->route('orders.index')->with('success', 'ordenes a sido creada correctamente.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Order $order)
     {
-        //
+        $order = Order::with('user')->findOrFail($order->id);
+        return view('orders.show', compact('order'));
     }
 
     /**
@@ -48,15 +55,20 @@ class OrderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $order = Order::with('user')->findOrFail($id);
+        $users = User::all();
+        $buy_verifications = Buy_Verification::all();
+        return view('orders.edit', compact('order', 'users', 'buy_verifications'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(OrderRequest $request, string $id)RedirectResponse
     {
-        //
+        $order = Order::with('user')->findOrFail($id);
+        $order->update($request->validated());
+        return redirect()->route('orders.index')->with('success', 'ordenes a sido actualizada correctamente.');
     }
 
     /**
@@ -64,6 +76,8 @@ class OrderController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $order = Order::with('user')->findOrFail($id);
+        $order->delete();
+        return redirect()->route('orders.index')->with('success', 'ordenes a sido eliminada correctamente.');
     }
 }
