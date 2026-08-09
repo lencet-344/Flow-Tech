0defb80bf0b8c1dba0fba3ac731864ed8a68d4ce
isaacmeneses231 <isaacmeneses254@gmail.com>
2026-08-08 19:31:14 -0600

    style: translate offers edit view to Spanish

--- resources/views/offers/edit.blade.php
+++ resources/views/offers/edit.blade.php
@@ -34,7 +34,7 @@
                         @enderror
                     </div>
                     <div class="mb-4">
-                        <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Description</label>
+                        <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Descripción</label>
                         <input type="text" name="description" id="description" value="{{ old('description', $offer->description) }}" class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                         @error('description')
                             <p class="mt-2 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
