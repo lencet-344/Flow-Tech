5cb3fc58e8891a64811e0b656dbd15dc311c9002
lencet-344 <odoricozeleherrera@gmail.com>
2026-08-15 14:03:43 -0600

    fix: update

---
+++ app/Models/Buy_verification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Buy_verification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'buy_verification';
+
+        protected $fillable = [
+        'quantity',
+        'date_buy',
+        'iva',
+        'cost_total',
+        'order_id',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
